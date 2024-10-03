4a782f0ff7b9565297a5dee69d99cd3777ecbe42
madhav <gmadhavganesh@gmail.com>
2024-10-03 18:46:42 +0530

    Basic Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Header from './Components/Header.js'
+import Nav from './Components/Nav.js'
+import Main from './Components/Main.js'
+import Footer from './Components/Footer.js'
 
 function App() {
   return (
-    <div className="App">
-      HomePage.
-    </div>
+    <>
+      <Header></Header>
+      <Nav />
+      <Main></Main>
+      <Footer></Footer>
+    </>
   );
 }
 
